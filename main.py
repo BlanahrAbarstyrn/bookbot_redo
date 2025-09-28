169570c6a3c05a78714f1a4508baab68559787c3
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-09-28 13:19:20 -0400

    all code in main, prints word count to terminal

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("greetings boots")
+# main.py
+
+def get_book_text(path_to_file):
+    file_contents = ""
+    
+    with open(path_to_file) as f:
+        file_contents = f.read()
+
+    return file_contents
+
+
+def word_count(file_contents):
+    word_list = []
+    word_qty = 0
+
+    word_list = file_contents.split()
+    word_qty = len(word_list)
+
+    return word_qty
+
+
+def main():
+    # note to self: relative path does not include the leading forward slash /
+    current_book = get_book_text("books/frankenstein.txt")
+    book_word_count = word_count(current_book)
+    print(f"Found {book_word_count} total words")
+
+
+main()
