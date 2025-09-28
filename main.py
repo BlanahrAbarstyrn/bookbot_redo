b108861b6660f1f3a4abd033ff7f888f87f782e7
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-09-28 14:06:17 -0400

    added letter count functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-from stats import get_num_words
+from stats import get_num_words, get_letter_count
 
 
 def get_book_text(path_to_file):
@@ -16,6 +16,8 @@ def main():
     current_book = get_book_text("books/frankenstein.txt")
     book_word_count = get_num_words(current_book)
     print(f"Found {book_word_count} total words")
+    book_letter_count = get_letter_count(current_book)
+    print(book_letter_count)
 
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,14 @@ def get_num_words(file_contents):
     word_qty = len(word_list)
 
     return word_qty
+
+
+def get_letter_count(file_contents):
+    letter_dictionary = {}
+    letters_list = list(file_contents.lower())
+    for letter in letters_list:
+        if letter not in letter_dictionary:
+            letter_dictionary[letter] = 1
+        else:
+            letter_dictionary[letter] += 1
+    return letter_dictionary
