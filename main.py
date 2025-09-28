8d3e68d49ebe751f69676e5ba56c647e2d053203
BlanahrAbarstyrn <ljspromo@protonmail.com>
2025-09-28 16:30:50 -0400

    add terminal input functionality to select book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # main.py
+import sys
 from stats import get_num_words, get_letter_count, sort_dictionary
 
 
@@ -10,9 +11,9 @@ def get_book_text(path_to_file):
 
     return file_contents
 
-def get_report(book_word_count, sorted_letter_count):
+def get_report(book_word_count, sorted_letter_count, book_path):
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     print(f"Found {book_word_count} total words")
     print("--------- Character Count -------")
@@ -23,12 +24,17 @@ def get_report(book_word_count, sorted_letter_count):
 
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     # note to self: relative path does not include the leading forward slash /
-    current_book = get_book_text("books/frankenstein.txt")
+    #current_book = get_book_text("books/frankenstein.txt")
+    current_book = get_book_text(book_path)
     book_word_count = get_num_words(current_book)
     book_letter_count = get_letter_count(current_book)
     sorted_letter_count = sort_dictionary(book_letter_count)
-    print_report = get_report(book_word_count, sorted_letter_count)
+    print_report = get_report(book_word_count, sorted_letter_count, book_path)
     return print_report
 
 
